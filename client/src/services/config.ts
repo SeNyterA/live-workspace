4c2c39d4d1df9c8c99017ce2518effc721eca904
NDKhang <SeNyterA@gmail.com>
2023-12-27 10:10:39 +0700

    reverse proxy socket

--- client/src/services/config.ts
+++ client/src/services/config.ts
@@ -1,6 +1,6 @@
 const { hostname, protocol } = window.location
 
-console.log({ hahahaha: window.location })
-
 export const baseURL =
   import.meta.env.VITE_API || `${protocol}//${hostname}/api`
+
+export const socketUrl = import.meta.env.VITE_SOCKET || `${protocol}//${hostname}`
