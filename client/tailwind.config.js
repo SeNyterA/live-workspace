8794ab70d9633b054e86272e7d22b4a7478982bb
NDKhang <SeNyterA@gmail.com>
2024-04-06 09:35:09 +0700

    setup theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
       boxShadow: {
         'custom': 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px',
       },
+      colors: {
+        primary: 'rgba(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+      },
     },
   },
   plugins: [],
