3f10abcb5fcc105e0ad06165324b0eb03b46b3a7
NDKhang <SeNyterA@gmail.com>
2024-04-21 23:09:57 +0700

    intro

--- client/src/components/landing/LandingContent.tsx
+++ client/src/components/landing/LandingContent.tsx
@@ -3,16 +3,29 @@ import './landing.scss'
 const Intro = () => {
   return (
     <div className='section relative h-screen bg-gray-950 text-gray-50'>
-      <p
-        data-text="&nbsp;SeNyter'A&nbsp;"
-        className='intro-text relative z-10 text-7xl font-bold text-transparent'
-      >
-        &nbsp;
-        <span className='text-9xl'>S</span>
-        <span>eNyter'A</span>
-        <sup className='text-xl'>.online</sup>
-        &nbsp;
-      </p>
+      <div className='intro relative'>
+        <p
+          data-text="&nbsp;SeNyter'A&nbsp;"
+          className='intro-text z-10 text-7xl font-bold text-transparent'
+        >
+          &nbsp;
+          <span className='text-9xl'>S</span>
+          <span>eNyter'A</span>
+          <sup className='text-xl'>.online</sup>
+          &nbsp;
+        </p>
+
+        <p
+          data-text="&nbsp;SeNyter'A&nbsp;"
+          className='intro-overlay absolute top-0 z-10 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 bg-clip-text text-7xl font-bold text-transparent'
+        >
+          &nbsp;
+          <span className='text-9xl'>S</span>
+          <span>eNyter'A</span>
+          <sup className='text-xl'>.online</sup>
+          &nbsp;
+        </p>
+      </div>
     </div>
   )
 }
