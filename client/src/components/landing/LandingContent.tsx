4d1092fca54ae37ce458cf4f16eeda92825d5388
NDKhang <SeNyterA@gmail.com>
2024-04-23 22:53:58 +0700

    fix

--- client/src/components/landing/LandingContent.tsx
+++ client/src/components/landing/LandingContent.tsx
@@ -1,68 +1,56 @@
 import { motion } from 'framer-motion'
 import './landing.scss'
+import { IconBrandGithub } from '@tabler/icons-react'
 
 const Intro = () => {
   return (
-    <>
-      <div className='content relative flex h-screen flex-col bg-black/80 text-black'>
-        <div />
-        <div className='absolute left-20'>
-          <div className='intro relative'>
-            <motion.div
-              data-text="&nbsp;SeNyter'A&nbsp;"
-              className='relative h-36 text-7xl font-bold text-transparent'
-              animate={
-                {
-                  // WebkitTextStrokeColor: ['#000', '#fff', '#000'],
-                  // WebkitTextStrokeWidth: '1px',
-                  // transition: {
-                  //   duration: 5,
-                  //   repeat: Infinity,
-                  //   repeatType: 'loop'
-                  // }
-                }
-              }
-            >
-              &nbsp;
-              <span className='text-9xl'>S</span>
-              <span>eNyter'A</span>
-              <sup className='text-xl'>.online</sup>
-              &nbsp;
-            </motion.div>
+    <div className='content flex h-screen flex-col bg-black/80 p-10 text-black'>
+      <div className='relative h-full w-full overflow-hidden rounded-[20px]'>
+        <div className='flex items-center justify-between gap-3 p-4 font-semibold'>
+          <motion.a className='flex flex-1 items-center rounded-xl border-none px-3 py-1.5 font-semibold text-slate-900 outline-0 no-underline' href='https://github.com/SeNyterA' >
+            <IconBrandGithub size={24} /> @SeNyterA
+          </motion.a>
 
-            <div
-              data-text="&nbsp;SeNyter'A&nbsp;"
-              className='intro-overlay absolute top-0 h-36 bg-gradient-to-r from-blue-400 via-yellow-500 to-indigo-900 bg-clip-text text-7xl font-bold text-transparent'
-            >
-              &nbsp;
-              <span className='text-9xl'>S</span>
-              <span>eNyter'A</span>
-              <sup className='text-xl'>.online</sup>
-              &nbsp;
-            </div>
-          </div>
+          <p>Login</p>
+          <motion.button
+            className='h-8 rounded-lg border-none bg-black px-3 text-gray-50 outline-0 ring-offset-2 hover:backdrop-blur-2xl hover:ring-1'
+            whileInView={{
+              scale: [1, 1.1, 1],
+              translateY: [-3, 0],
+              transition: { duration: 0.4 }
+            }}
+          >
+            Register
+          </motion.button>
         </div>
+        <motion.div
+          data-text="&nbsp;SeNyter'A&nbsp;"
+          className='absolute left-20 top-[50%] h-36 bg-gradient-to-r from-blue-900 via-yellow-900 to-indigo-900 bg-clip-text text-7xl font-bold text-transparent hover:drop-shadow-[1000px]'
+          initial={{ translateY: '-50%' }}
+          whileInView={{
+            scale: [1, 1.1, 1],
+            transition: { duration: 0.4 }
+          }}
+        >
+          &nbsp;
+          <span className='text-9xl'>S</span>
+          <span>eNyter'A</span>
+          <sup className='text-xl'>.online</sup>
+          &nbsp;
+        </motion.div>
       </div>
-    </>
+    </div>
   )
 }
 
 const Features = () => {
   return (
-    <motion.div
-      data-none
-      className='content relative'
-      // initial={{ opacity: 0, backgroundColor: '#fff' }}
-      // whileInView={{ opacity: 1, backgroundColor: '#000000' }}
-    >
-      <div data-none />
-      <div className='absolute bottom-10 left-10 max-w-[600px] rounded-[20px] bg-red-500  p-10 text-5xl leading-[110%]'>
-        <p className='text-6xl font-semibold'>Live Workspace</p>
-        <mark className='rounded-lg p-2'>
-          The simple workspace for everyone easy to use
-        </mark>
+    <div className='content relative flex h-screen flex-col bg-black/80 text-black'>
+      <div />
+      <div className='text-5xl font-semibold mix-blend-luminosity'>
+        <p>Live workspace</p>
       </div>
-    </motion.div>
+    </div>
   )
 }
 
