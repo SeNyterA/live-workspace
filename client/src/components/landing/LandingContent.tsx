b078556bbf4fe47b86f2457b5adaa4ce69e2fbba
NDKhang <SeNyterA@gmail.com>
2024-04-24 00:05:49 +0700

    tmp

--- client/src/components/landing/LandingContent.tsx
+++ client/src/components/landing/LandingContent.tsx
@@ -2,34 +2,44 @@ import { motion } from 'framer-motion'
 import './landing.scss'
 import { IconBrandGithub } from '@tabler/icons-react'
 
-const Intro = () => {
-  return (
-    <div className='content flex h-screen flex-col bg-black/80 p-10 text-black'>
-      <div className='relative h-full w-full overflow-hidden rounded-[20px]'>
-        <div className='flex items-center justify-between gap-3 p-4 font-semibold'>
-          <motion.a className='flex flex-1 items-center rounded-xl border-none px-3 py-1.5 font-semibold text-slate-900 outline-0 no-underline' href='https://github.com/SeNyterA' >
-            <IconBrandGithub size={24} /> @SeNyterA
-          </motion.a>
+const Intro = () => (
+  <div className='content flex h-screen flex-col bg-black/80 p-10 text-black'>
+    <div className='relative h-full w-full overflow-hidden rounded-[20px]'>
+      <div className='flex items-center justify-between gap-3 p-4 font-semibold'>
+        <motion.a
+          className='flex flex-1 items-center rounded-xl border-none px-3 py-1.5 font-semibold text-slate-900 no-underline outline-0'
+          href='https://github.com/SeNyterA'
+        >
+          <IconBrandGithub size={24} /> @SeNyterA
+        </motion.a>
 
-          <p>Login</p>
-          <motion.button
-            className='h-8 rounded-lg border-none bg-black px-3 text-gray-50 outline-0 ring-offset-2 hover:backdrop-blur-2xl hover:ring-1'
-            whileInView={{
-              scale: [1, 1.1, 1],
-              translateY: [-3, 0],
-              transition: { duration: 0.4 }
-            }}
-          >
-            Register
-          </motion.button>
-        </div>
-        <motion.div
-          data-text="&nbsp;SeNyter'A&nbsp;"
-          className='absolute left-20 top-[50%] h-36 bg-gradient-to-r from-blue-900 via-yellow-900 to-indigo-900 bg-clip-text text-7xl font-bold text-transparent hover:drop-shadow-[1000px]'
-          initial={{ translateY: '-50%' }}
+        <p>Login</p>
+        <motion.button
+          className='h-8 rounded-lg border-none bg-black px-3 text-gray-50 outline-0 ring-offset-2 hover:ring-1 hover:backdrop-blur-2xl'
           whileInView={{
             scale: [1, 1.1, 1],
+            translateY: [-3, 0],
             transition: { duration: 0.4 }
+          }}
+        >
+          Register
+        </motion.button>
+      </div>
+
+      <motion.div
+        data-text="&nbsp;SeNyter'A&nbsp;"
+        className='relative text-7xl font-bold'
+        // className='absolute left-20 top-[50%] h-36 bg-gradient-to-r from-blue-900 via-yellow-900 to-indigo-900 bg-clip-text text-7xl font-bold text-transparent hover:drop-shadow-[1000px]'
+
+        whileInView={{
+          scale: [1, 1.01, 1],
+          transition: { duration: 0.4 }
+        }}
+      >
+        <div
+          className='absolute bg-clip-text text-transparent'
+          style={{
+            WebkitTextStroke: '.5px #000'
           }}
         >
           &nbsp;
@@ -37,11 +47,18 @@ const Intro = () => {
           <span>eNyter'A</span>
           <sup className='text-xl'>.online</sup>
           &nbsp;
-        </motion.div>
-      </div>
+        </div>
+        <div className='absolute w-0'>
+          &nbsp;
+          <span className='text-9xl'>S</span>
+          <span>eNyter'A</span>
+          <sup className='text-xl'>.online</sup>
+          &nbsp;
+        </div>
+      </motion.div>
     </div>
-  )
-}
+  </div>
+)
 
 const Features = () => {
   return (
