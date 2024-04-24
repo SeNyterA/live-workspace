b3c76d8a5861cc1dcc9c6e95b6a2a995d451ee52
NDKhang <SeNyterA@gmail.com>
2024-04-24 14:52:05 +0700

    fix landing

--- client/src/components/landing/LandingContent.tsx
+++ client/src/components/landing/LandingContent.tsx
@@ -1,72 +1,91 @@
-import { motion } from 'framer-motion'
+import { motion, useMotionValue, useTransform } from 'framer-motion'
 import './landing.scss'
-import { IconBrandGithub } from '@tabler/icons-react'
+import { IconBrandGithub, IconX } from '@tabler/icons-react'
 
-const Intro = () => (
-  <div className='content flex h-screen flex-col bg-black/80 p-10 text-black'>
-    <div className='relative h-full w-full overflow-hidden rounded-[20px]'>
-      <div className='flex items-center justify-between gap-3 p-4 font-semibold'>
-        <motion.a
-          className='flex flex-1 items-center rounded-xl border-none px-3 py-1.5 font-semibold text-slate-900 no-underline outline-0'
-          href='https://github.com/SeNyterA'
+const Intro = () => {
+  const x = useMotionValue(0)
+  const background = useTransform(
+    x,
+    [-100, 0, 100],
+    ['#ff008c', '#7700ff', 'rgb(230, 255, 0)']
+  )
+
+  return (
+    <div className='content flex h-screen flex-col bg-black/80 p-10 text-black'>
+      <div className='relative h-full w-full overflow-hidden rounded-[20px]'>
+        <div className='flex items-center justify-between gap-3 p-4 font-semibold'>
+          <motion.a
+            className='flex flex-1 items-center rounded-xl border-none px-3 py-1.5 font-semibold text-slate-900 no-underline outline-0'
+            href='https://github.com/SeNyterA'
+          >
+            <IconBrandGithub size={24} /> @SeNyterA
+          </motion.a>
+
+          <p>Login</p>
+          <motion.button
+            className='h-8 rounded-lg border-none bg-black px-3 text-gray-50 outline-0 ring-offset-2 hover:ring-1 hover:backdrop-blur-2xl'
+            whileInView={{
+              scale: [1, 1.1, 1],
+              translateY: [-3, 0],
+              transition: { duration: 0.4 }
+            }}
+          >
+            Register
+          </motion.button>
+        </div>
+
+        <motion.div
+          style={{ background, translateX: '-50%' }}
+          className='absolute bottom-10 left-1/2 z-10 h-12 w-12 rounded-2xl bg-blend-screen backdrop-brightness-105'
         >
-          <IconBrandGithub size={24} /> @SeNyterA
-        </motion.a>
+          <motion.div
+            className='flex h-12 w-12 items-center justify-center rounded-2xl'
+            drag='x'
+            dragConstraints={{ left: 0, right: 0 }}
+            style={{ x }}
+            dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+          >
+            <IconX size={40} />
+          </motion.div>
+        </motion.div>
 
-        <p>Login</p>
-        <motion.button
-          className='h-8 rounded-lg border-none bg-black px-3 text-gray-50 outline-0 ring-offset-2 hover:ring-1 hover:backdrop-blur-2xl'
-          whileInView={{
-            scale: [1, 1.1, 1],
-            translateY: [-3, 0],
-            transition: { duration: 0.4 }
-          }}
-        >
-          Register
-        </motion.button>
-      </div>
-
-      <motion.div
-        data-text="&nbsp;SeNyter'A&nbsp;"
-        className='relative text-7xl font-bold'
-        // className='absolute left-20 top-[50%] h-36 bg-gradient-to-r from-blue-900 via-yellow-900 to-indigo-900 bg-clip-text text-7xl font-bold text-transparent hover:drop-shadow-[1000px]'
-
-        whileInView={{
-          scale: [1, 1.01, 1],
-          transition: { duration: 0.4 }
-        }}
-      >
-        <div
-          className='absolute bg-clip-text text-transparent'
+        <motion.div
+          className='intro left-10 top-1/2'
           style={{
-            WebkitTextStroke: '.5px #000'
+            translateY: '-50%'
+          }}
+          initial={{ opacity: 0, translateX: -100 }}
+          whileInView={{
+            scale: [1, 1.05, 1],
+            transition: { duration: 0.5 },
+            opacity: 1,
+            translateX: 0
           }}
         >
-          &nbsp;
-          <span className='text-9xl'>S</span>
-          <span>eNyter'A</span>
-          <sup className='text-xl'>.online</sup>
-          &nbsp;
-        </div>
-        <div className='absolute w-0'>
-          &nbsp;
-          <span className='text-9xl'>S</span>
-          <span>eNyter'A</span>
-          <sup className='text-xl'>.online</sup>
-          &nbsp;
-        </div>
-      </motion.div>
+          <div className='intro-text'>
+            &nbsp;
+            <span className='text-9xl'>S</span>
+            <span>eNyter'A</span>
+            <sup className='text-3xl'>.online</sup>
+            &nbsp;
+          </div>
+          <div className='intro-text-overlay'>
+            &nbsp;
+            <span className='text-9xl'>S</span>
+            <span>eNyter'A</span>
+            <sup className='text-3xl'>.online</sup>
+            &nbsp;
+          </div>
+        </motion.div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 const Features = () => {
   return (
-    <div className='content relative flex h-screen flex-col bg-black/80 text-black'>
-      <div />
-      <div className='text-5xl font-semibold mix-blend-luminosity'>
-        <p>Live workspace</p>
-      </div>
+    <div className='content bg-clip-padding absolute inset-0'>
+      <div data-none className='rounded-[20px] bg-black/90 absolute inset-10'></div>
     </div>
   )
 }
@@ -74,7 +93,7 @@ const Features = () => {
 export default function LandingContent() {
   return (
     <>
-      <Intro />
+      {/* <Intro /> */}
       <Features />
     </>
   )
