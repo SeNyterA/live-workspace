fae64545afacb4624b0e8a163cbc3d783f033bfc
NDKhang <SeNyterA@gmail.com>
2024-04-25 13:25:52 +0700

    carousel

--- client/src/components/landing/Silde.tsx
+++ client/src/components/landing/Silde.tsx
@@ -1,5 +1,5 @@
 import { motion, useMotionValue } from 'framer-motion'
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { useState } from 'react'
 
 const imgs = [
   '/imgs/nature/1.jpg',
@@ -11,8 +11,6 @@ const imgs = [
   '/imgs/nature/7.jpg'
 ]
 
-const ONE_SECOND = 1000
-const AUTO_DELAY = ONE_SECOND * 10
 const DRAG_BUFFER = 50
 
 const SPRING_OPTIONS = {
@@ -24,26 +22,7 @@ const SPRING_OPTIONS = {
 
 export const SwipeCarousel = () => {
   const [imgIndex, setImgIndex] = useState(0)
-
   const dragX = useMotionValue(0)
-
-  // useEffect(() => {
-  //   const intervalRef = setInterval(() => {
-  //     const x = dragX.get()
-
-  //     if (x === 0) {
-  //       setImgIndex(pv => {
-  //         if (pv === imgs.length - 1) {
-  //           return 0
-  //         }
-  //         return pv + 1
-  //       })
-  //     }
-  //   }, AUTO_DELAY)
-
-  //   return () => clearInterval(intervalRef)
-  // }, [])
-
   const onDragEnd = () => {
     const x = dragX.get()
 
@@ -66,76 +45,32 @@ export const SwipeCarousel = () => {
           x: dragX
         }}
         animate={{
-          translateX: `calc(-${imgIndex * 100}% - ${imgIndex * 8}px)`
+          translateX: `calc(-${imgIndex * 100}% - ${imgIndex * 16}px)`
         }}
         transition={SPRING_OPTIONS}
         onDragEnd={onDragEnd}
-        className='flex h-full cursor-grab items-center space-x-2 active:cursor-grabbing'
+        className='flex h-full cursor-grab items-center space-x-4 active:cursor-grabbing'
       >
-        <Images imgIndex={imgIndex} />
-      </motion.div>
-
-      {/* <Dots imgIndex={imgIndex} setImgIndex={setImgIndex} />
-      <GradientEdges /> */}
-    </div>
-  )
-}
-
-const Images = ({ imgIndex }: { imgIndex: number }) => {
-  return (
-    <>
-      {imgs.map((imgSrc, idx) => {
-        return (
-          <motion.div
-            key={idx}
-            style={{
-              backgroundImage: `url(${imgSrc})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-            }}
-            animate={
-              {
-                // scale: imgIndex === idx ? 0.95 : 0.85
+        {imgs.map((imgSrc, idx) => {
+          return (
+            <motion.div
+              key={idx}
+              style={{
+                backgroundImage: `url(${imgSrc})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center'
+              }}
+              animate={
+                {
+                  scale: imgIndex === idx ? 1 : 0.97
+                }
               }
-            }
-            transition={SPRING_OPTIONS}
-            className=' h-full w-full shrink-0 rounded-xl bg-neutral-800 object-cover'
-          />
-        )
-      })}
-    </>
-  )
-}
-
-const Dots = ({
-  imgIndex,
-  setImgIndex
-}: {
-  imgIndex: number
-  setImgIndex: Dispatch<SetStateAction<number>>
-}) => {
-  return (
-    <div className='mt-4 flex w-full justify-center gap-2'>
-      {imgs.map((_, idx) => {
-        return (
-          <button
-            key={idx}
-            onClick={() => setImgIndex(idx)}
-            className={`h-3 w-3 rounded-full transition-colors ${
-              idx === imgIndex ? 'bg-neutral-50' : 'bg-neutral-500'
-            }`}
-          />
-        )
-      })}
+              transition={SPRING_OPTIONS}
+              className='h-full w-full shrink-0 rounded-xl bg-neutral-800 object-cover'
+            />
+          )
+        })}
+      </motion.div>
     </div>
-  )
-}
-
-const GradientEdges = () => {
-  return (
-    <>
-      <div className='pointer-events-none absolute bottom-0 left-0 top-0 w-[10vw] max-w-[100px] bg-gradient-to-r from-neutral-950/50 to-neutral-950/0' />
-      <div className='pointer-events-none absolute bottom-0 right-0 top-0 w-[10vw] max-w-[100px] bg-gradient-to-l from-neutral-950/50 to-neutral-950/0' />
-    </>
   )
 }
